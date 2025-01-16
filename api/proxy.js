e1e23b6270448bc211c4892319ba0af6809dbb30
shnulaa <ashitawillbefine@gmail.com>
2025-01-16 20:01:22 +0800

    Update proxy.js1111111111

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://panel.liuyq.us.kg/";//your website url
+  let target = "https://panel-bk.liuyq.us.kg/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
