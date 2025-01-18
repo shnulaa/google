c9cefed848b2a0c8e8696c9fb53ed6619a1763d6
shnulaa <ashitawillbefine@gmail.com>
2025-01-18 19:15:02 +0800

    Update proxy.js34

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.liuyq.us.kg/";//your website url
+  let target = "https://mwiki.liuyq.xyz/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
